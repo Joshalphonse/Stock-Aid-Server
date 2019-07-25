94e48f6284ec4b2d84d8c1893d36bcd5587eac0e
sbrij001 <sbrij001@yahoo.com>
2019-07-25 10:43:16 -0400

    react router and created Home Page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Redirect, withRouter } from "react-router-dom";
+
+
+const Home = props => {
+  return (
+    <div>
+        <h1>Landing Page</h1>
+    </div>
+  );
+};
+
+export default withRouter(Home);
